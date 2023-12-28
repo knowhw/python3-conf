288b6e5156ae1b1d0acbd3c259ff7a2f5e2f3612
Nuri Mete <142631271+knowhw@users.noreply.github.com>
2023-12-28 07:10:23 +0000

    Update __init__.py

--- conf/__init__.py
+++ conf/__init__.py
@@ -4,31 +4,38 @@ from conf import module
 
 
 def read(key):
+	
 	key = [item for item in key.split(chr(47)) if item]
 	
 	read = module.popen("%s %s" % (module.attrib.read, "/%s" % chr(47).join(key))).read()
-	return read.strip() [1:-1:]
+	return read.strip() 
 def write(key, value):
 	
 	
 	return module.write("%s %s/ %s" % (module.attrib.write, value))
 def list(directory):
-	keylist = module.popen("%s %s" % (module.attrib.list, directory)).readlines()
+	
+	dirs = [item for item in directory.split(chr(47)) if item]
+	keylist = module.popen("%s %s" % (module.attrib.list, "/%s/" % chr(47).join(dirs))).readlines()
+	
 	
 	
 	return [key.strip() for key in keylist]
+def load(directory, dumpf):
+	""" dumpf: database.db """
+	module.write("%s %s < %s" % (module.attrib.load, directory, dumpf))
+	
 def dump(directory):
 	return module.popen("%s %s" % (module.attrib.dump, directory)).read()
-def export(path, dumps):
-	with open(path, "w") as f : 
-		f.write(dumps)
 	
+def export(path, dump):
+	with open(path, "w") as f : 
+		f.write(dump)
 
 
 
 
 
-""" dconf read [-d] KEY  """
-""" dconf write KEY VALUE  """
-""" dconf list DIR """
-""" dconf dump DIR """
+
+
+
