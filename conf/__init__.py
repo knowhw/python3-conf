6856d4025051691b894df94e686cdea945e17b46
knowhw <142631271+knowhw@users.noreply.github.com>
2023-12-30 18:51:09 +0000

    Update __init__.py

--- conf/__init__.py
+++ conf/__init__.py
@@ -6,21 +6,23 @@ from conf import module
 
 def read(key):
 	
-	
 	key = [item for item in key.split(chr(47)) if item]
 	
-	read = module.popen("%s %s" % (module.attrib.read, "/%s" % chr(47).join(key))).read()
-	return read.strip() 
+	return module.popen("%s %s" % (module.attrib.read, "/%s" % chr(47).join(key))).read().strip() 
 def write(key, value):
+	
+	key = [item for item in key.split(chr(47)) if item]
+	key = "/%s" % chr(47).join(key)
+	
 	
 	value = module.dumps(str(value))
 	
 	if value == '"False"': value = "false"
 	elif value == '"True"': value = "true"
-	
-	
-	return module.write("%s %s '%s'" % (module.attrib.write, key, value))
-def list(directory):
+		
+
+	return module.write("%s %s %s" % (module.attrib.write, key, value))
+def listkey(directory):
 	
 	dirs = [item for item in directory.split(chr(47)) if item]
 	keylist = module.popen("%s %s" % (module.attrib.list, "/%s/" % chr(47).join(dirs))).readlines()
